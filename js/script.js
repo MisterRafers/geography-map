35dac70c1155be8ea2523894f09322363a56b6ed
CerBor <87604402+MisterRafers@users.noreply.github.com>
2024-03-22 23:24:41 +0700

    Revert "Pre-pre final commit"
    This reverts commit 90ea185bb03c7be05d8162f5d14c39c563a2e51e.

--- js/script.js
+++ js/script.js
@@ -397,7 +397,6 @@ document.getElementById('summitBtn').addEventListener('click', () => {
 
     document.getElementById('grade').innerHTML = 'Оценка: ' + getGrade(correct / questionsAnswered.size);
 
-    Chart.defaults.font.size = 16;
     new Chart(ctx, {
         type: 'pie',
         data: {
